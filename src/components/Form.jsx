25c7790dbc9b0457265d7d9c529cee5071fc0c06
Akshad Gawde <gawdeakshad@gmail.com>
2025-03-11 22:02:56 +0530

    fix 4

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,54 +1,14 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import emailjs from "emailjs-com";
 import toast from "react-hot-toast";
 
-const subjects = [
-  "Maths-1",
-  "Maths-2",
-  "Engineering Graphics/Design",
-  "Basic Electrical Engineering",
-  "Engineering Mechanics",
-  "Maths-3",
-  "Maths-4",
-  "Electrical network/ circuit theory/ network theory (EN/NT/CTN)",
-  "Control system (CS)",
-  "Digital logic design/ digital system design/ digital circuits design (DSD/ DLDA/DCD/LD)",
-  "Electronics circuit design (EDC/ADC/ED)",
-  "Signal and system (SS)",
-  "Linear integrated circuit/ Integrated circuit (LIC/DLIC/IC)",
-  "Electromagnetics (EME)",
-  "Digital signal processing/ discrete time signal processing (DSP/DTSP)",
-  "Signal processing (SP)",
-  "Very large scale integrated circuit (VLSI)",
-  "image processing (IP)",
-  "Robotics",
-  "CMOS technology",
-];
-
 export const Form = () => {
-  const { register, handleSubmit , reset } = useForm();
+  const { register, handleSubmit, reset } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
-    emailjs
-      .sendForm(
-        "service_b9kcp18",
-        "template_zt441ac",
-        "#myForm",
-        "yDUofkTF6aoIqQiG_"
-      )
-      .then(
-        (response) => {
-          console.log("SUCCESS!", response.status, response.text);
-          toast.success("Form has been submitted");
-        },
-        (error) => {
-          console.log("FAILED...", error);
-          toast.error("Form could not be submitted , Please try again");
-        }
-      );
-      reset();
+    toast.success("Form has been submitted");
+    reset();
   };
 
   return (
